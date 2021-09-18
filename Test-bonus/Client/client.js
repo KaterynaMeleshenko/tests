e8220d0939235366e0186673525a87f3484a662f
Kateryna Meleshenko <yekaterynamel@gmail.com>
2021-09-18 17:39:05 +0300

    async func changed

--- Test-bonus/Client/client.js
+++ Test-bonus/Client/client.js
@@ -1,26 +1,24 @@
-function getVideos() {
+async function getVideos() {
 
-    document.body.insertAdjacentHTML('beforeend', `<div id="videos"></div>`);
-    const mainInfo = document.getElementById('videos');
-  
-      axios.get(`http://localhost:3000/`)
-        .then(res => {
-          const videosData = res.data;
-          let number = 1;
-  
-          videosData.forEach(video => {
-          const title = video.title; 
-          const date = video.date;   
+  document.body.insertAdjacentHTML('beforeend', `<div id="videos"></div>`);
+  const mainInfo = document.getElementById('videos');
+      
+    try {
+      const result = await axios.get(`http://localhost:3000/`);
+      const videosData = result.data;
+      let number = 1;
+      videosData.forEach(video => {
+        const title = video.title; 
+        const date = video.date;   
 
-          mainInfo.insertAdjacentHTML('beforeend', `<div class="video-info">Video #${number}. <b>Title</b>: 
-            ${title} (<b>date of publication:</b> ${date})</div>`)
+        mainInfo.insertAdjacentHTML('beforeend', `<div class="video-info">Video #${number}. <b>Title</b>: 
+          ${title} (<b>date of publication:</b> ${date})</div>`)
   
-          number += 1
-          })
-        })
-        .catch(error => {
+        number += 1
+      })
+    } catch (err) {
           console.log("error", error)
-        })
+    }
 }
   
 getVideos();
